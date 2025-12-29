1b4b5f6d17ebcced2fae5fb426cc6a59b388da46
hehehahagugugaga <168170823+hehehahagugugaga@users.noreply.github.com>
2025-12-29 17:53:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import random
+
+web = Flask(__name__)
+
+@web.route('/')
+def hola():
+    return render_template('index.html', result="")
+
+@web.route('/s', methods=['POST'])
+def second():
+    l = int(request.form.get('ln'))
+    u = int(request.form.get('un'))
+    n = int(request.form.get('tn'))
+
+    op = []
+    while len(op) < n:
+        j = random.randint(l, u)
+        if j not in op:
+            op.append(j)
+
+    return render_template('index.html', result=", ".join(map(str, op)))
+
+
+if __name__ == '__main__':
+    web.run(debug=True)
